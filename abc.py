cba7447b50a1bd07a0a5dab3993992049078f8e8
ItachiUchiha001 <32765926+ItachiUchiha001@users.noreply.github.com>
2017-10-13 16:03:50 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,1 +1,12 @@
-
+string=input("Enter Statement:")
+vowels=0
+consonants=0
+lengthofwords=len(string)
+for i in string:
+    if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+        vowels=vowels+1
+        consonants=lengthofwords-vowels
+print("Number of vowels are:")
+print(vowels)
+print("Number of consonants are:")
+print(consonants)
